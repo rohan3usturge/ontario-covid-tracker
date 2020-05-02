07d4daf94b70aedd49697689733f2c6b4fe6e7c5
Rohan Usturge <rohan.usturge@hotmail.com>
2020-05-02 13:12:10 -0400

    feat: first setup

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Tile from "./tile.svelte";
+  import BarChart from "./bar-chart.svelte";
+  const data = [
+    {
+      name: "Total Cases",
+      count: 2000,
+      textStyle: "info"
+    },
+    {
+      name: "New Cases",
+      count: 2000,
+      textStyle: "warning"
+    },
+    {
+      name: "Deaths",
+      count: 2000,
+      textStyle: "danger"
+    },
+    {
+      name: "Recovered",
+      count: 2000,
+      textStyle: "success"
+    }
+  ];
+</script>
+
+<main>
+  <div class="container-fluid">
+    <h5 class="text-uppercase">
+      Ontario
+      <small>
+        <em>Covid Tracker</em>
+      </small>
+    </h5>
+    <div style="margin-top: 25px;" />
+    <div class="row">
+      {#each data as { name, count, textStyle }}
+        <div class="col-sm-6 col-md-4 col-lg-2">
+          <Tile {name} {count} {textStyle} />
+        </div>
+      {/each}
+    </div>
+    <div style="margin-top: 25px;" />
+    <BarChart />
+  </div>
+</main>
---
+++ src/bar-chart.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+  var data = [30, 86, 168, 281, 303, 365];
+
+  let el;
+
+  onMount(() => {
+    d3.select(el)
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .classed("chart", true)
+      .style("height", function(d) {
+        return d + "px";
+      })
+      .style("width", "50px")
+      .text(function(d) {
+        return d;
+      });
+  });
+</script>
+
+<style>
+  .chart-container {
+    flex-direction: row;
+    display: flex;
+    align-items: flex-end;
+  }
+</style>
+
+<div class="card">
+  <div bind:this={el} class="chart-container" />
+</div>
