e529f26e5ca77b44380df6d34f7eb1f318ec10f5
Rohan Usturge <rohan.usturge@hotmail.com>
2020-05-03 05:21:18 -0400

    fix: about us

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -2,15 +2,9 @@
   <div class="container">
     <h1 class="display-4">Contact Us</h1>
     <p class="lead">
-      The site has been made purely to surface covid-19 information. This a
-      public, non-profie site and has no intention to take make any commercial
-      gains.
+      The site has been made purely to surface official covid-19 information
+      with a few search capabilities. This a public, non-profit site and has no
+      intention to make any commercial gains.
     </p>
-    <a
-      class="btn btn-primary btn-lg"
-      href="https://github.com/rohan3usturge"
-      role="button">
-      Find me here
-    </a>
   </div>
 </div>
