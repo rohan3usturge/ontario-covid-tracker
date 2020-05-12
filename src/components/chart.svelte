03e524a38aa6a0b66ba4f7c64bc76599502240cb
Rohan Usturge <rohan.usturge@hotmail.com>
2020-05-12 01:01:24 -0400

    fixes

--- src/components/chart.svelte
+++ src/components/chart.svelte
@@ -29,6 +29,15 @@
     dataFormat: "json",
     dataSource: dataSource(value)
   });
+  jQuery.event.special.touchstart = {
+    setup: function(_, ns, handle) {
+      if (ns.includes("noPreventDefault")) {
+        this.addEventListener("touchstart", handle, { passive: false });
+      } else {
+        this.addEventListener("touchstart", handle, { passive: true });
+      }
+    }
+  };
 </script>
 
 <div class="card">
